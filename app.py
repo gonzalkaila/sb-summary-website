beae6b0b3df4f9802ec3795b2dcb3208db6b4334
gonzalkaila <138711641+gonzalkaila@users.noreply.github.com>
2023-07-28 19:44:03 +0000

    final changes

--- app.py
+++ app.py
@@ -4,57 +4,74 @@ app = Flask(__name__)
 
 FAPRODUCTS = [
   {
-    'id': 1,
-    'name': 'Advanced Choice Annuity Rates'
+    'id': 0,
+    'name': 'Advanced Choice Annuity Rates',
+    'url': 'https://www.securitybenefit.com/AdvancedChoiceRates'
   },
   {
+    'id': 1,
+    'name': 'Choice Annuity Renewal Rates',
+    'url': 'https://www.securitybenefit.com/ChoiceAnnuityRates'
+  },
+{
     'id': 2,
-    'name': 'Choice Annuity Renewal Rates'
+    'name': 'RateTrack Annuity Rates',
+    'url': 'https://www.securitybenefit.com/RateTrackRates'
   },
 {
     'id': 3,
-    'name': 'RateTrack Annuity Rates'
-  },
-{
-    'id': 4,
-    'name': 'Total Interest Annuity Rates'
+    'name': 'Total Interest Annuity Rates',
+    'url': 'https://www.securitybenefit.com/TIARates'
   }
 ]
 FIAPRODUCTS = [
   {
-    'id': 5,
-    'name': 'ClearLine Annuity Rates'
+    'id': 4,
+    'name': 'ClearLine Annuity Rates',
+    'url': 'https://www.securitybenefit.com/ClearLineRates'
   },
   {
-    'id': 6,
-    'name': 'Foundations Annuity Rates'
+    'id': 5,
+    'name': 'Foundations Annuity Rates',
+    'url': 'https://www.securitybenefit.com/FoundationsRates'
   }
 ]
 VAPRODUCTS = [
   {
-    'id': 7,
+    'id': 6,
     'name': 'AdvanceDesigns Variable Annuity Rates',
-    'type': 'Variable Annuity'
+    'type': 'Variable Annuity',
+    'url': 'https://www.securitybenefit.com/rates/advancedesigns-variable-annuity-rates'
+  },
+  {
+    'id': 7,
+    'name': 'Advisor 403(b) Program Rates',
+    'type': 'Variable Annuity',
+    'url': 'https://www.securitybenefit.com/rates/advisor-403b-program-interest-rates'
   },
   {
     'id': 8,
     'name': 'AEA Valuebuilder Variable Annuity Rates',
-    'type': 'Variable Annuity'
+    'type': 'Variable Annuity',
+    'url': 'https://www.securitybenefit.com/rates/aea-valuebuilder-variable-annuity-rates'
   },
   {
     'id': 9,
     'name': 'NEA Valuebuilder Variable Annuity Rates',
-    'type': 'Variable Annuity'
+    'type': 'Variable Annuity',
+    'url': 'https://www.securitybenefit.com/rates/nea-valuebuilder-variable-annuity-rates'
   },
   {
     'id': 10,
     'name': 'SecureDesigns Variable Annuity Rates',
-    'type': 'Variable Annuity'
+    'type': 'Variable Annuity',
+    'url': 'https://www.securitybenefit.com/rates/securedesigns-variable-annuity-rates'
   },
   {
     'id': 11,
     'name': 'Variflex Variable Annuity Rates',
-    'type': 'Variable Annuity'
+    'type': 'Variable Annuity',
+    'url': 'https://www.securitybenefit.com/rates/variflex-variable-annuity-rates'
   }
 ]
 
